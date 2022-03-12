df92e0b6e47dd69f2b18843ad4858850468f2531
HilmiMubarok <m.hilmimubarok@gmail.com>
2022-03-12 20:20:19 +0700

    add middleware folder and simple logger

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const app = express();
 const router = require('./routes');
+const log = require('./middleware/logger');
 
+app.use(log);
 app.use(router);
 
 app.listen(3333, () => console.log('Server started on port 3333'));

4b5fe1fb22e3c5ddf5a4178356a7c307548fea0e
HilmiMubarok <m.hilmimubarok@gmail.com>
2022-03-12 20:29:51 +0700

    add 404 route

--- app.js
+++ app.js
@@ -7,4 +7,12 @@ const log = require('./middleware/logger');
 app.use(log);
 app.use(router);
 
+// 404 Not Found Route
+app.use((req, res, next) => {
+	res.send({
+		status: 404,
+		message: 'Not Found'
+	});
+});
+
 app.listen(3333, () => console.log('Server started on port 3333'));

eef3261271526e91544fe21324876c143f8e1805
HilmiMubarok <m.hilmimubarok@gmail.com>
2022-03-12 20:03:38 +0700

    add dynamic route

--- routes.js
+++ routes.js
@@ -1,9 +1,29 @@
+const { route } = require('express/lib/application');
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
 	res.send({
 		status: 'success',
 		message: 'Hello World'
+	});
+});
+
+// Dynamic route
+router.get('/user/:id', (req, res) => {
+	const { id } = req.params;
+	res.json({
+		status: 'success',
+		message: `Hello User ${id}`
+	});
+});
+
+router.get('/:category/:tag', (req, res) => {
+	const { category, tag } = req.params;
+	res.json({
+		status: 'success',
+		category,
+		tag
 	});
 });
 

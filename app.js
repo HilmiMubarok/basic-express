c21b61ac736c1de38698f26862463bfd69ac7514
HilmiMubarok <m.hilmimubarok@gmail.com>
2022-03-17 14:57:45 +0700

    change port

--- app.js
+++ app.js
@@ -30,4 +30,4 @@ app.use((req, res, next) => {
 	});
 });
 
-app.listen(4000, () => console.log('Server started on port 4000'));
+app.listen(process.env.PORT || 5000, () => console.log('Server started'));

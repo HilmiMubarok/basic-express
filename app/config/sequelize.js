d6bfb819e83c25dd6e3fb4de32ddf04749d9a142
HilmiMubarok <m.hilmimubarok@gmail.com>
2022-04-05 21:37:20 +0700

    add config pg heroku

--- app/config/sequelize.js
+++ app/config/sequelize.js
@@ -1,20 +1,37 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-	dialect: 'mysql',
-	host: 'localhost',
-	username: 'root',
-	password: '',
-	database: 'expressdb'
-});
-
-(async () => {
-	try {
-		await sequelize.authenticate();
-		console.log('Connection has been established successfully.');
-	} catch (error) {
-		console.error('Unable to connect to the database:', error);
-	}
-})();
-
-module.exports = sequelize;
+if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+	const sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+		dialect: 'postgres',
+		protocol: 'postgres',
+		port: 5432,
+		host: 'ec2-3-223-213-207.compute-1.amazonaws.com',
+		logging: true //false
+	});
+	(async () => {
+		try {
+			await sequelize.authenticate();
+			console.log('Connection has been established successfully.');
+		} catch (error) {
+			console.error('Unable to connect to the database:', error);
+		}
+	})();
+	module.exports = sequelize;
+} else {
+	const sequelize = new Sequelize({
+		dialect: 'mysql',
+		host: 'localhost',
+		username: 'root',
+		password: '',
+		database: 'expressdb'
+	});
+	(async () => {
+		try {
+			await sequelize.authenticate();
+			console.log('Connection has been established successfully.');
+		} catch (error) {
+			console.error('Unable to connect to the database:', error);
+		}
+	})();
+	module.exports = sequelize;
+}
